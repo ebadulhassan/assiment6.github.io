ac3aa7941d2cf4c0276b7a8478e7d8dcb4862b14
ebadulhassan <ebadulhassan967@gmail.com>
2023-05-21 15:08:53 -0700

    6assigment

---
+++ index.js
@@ -0,0 +1,20 @@
+// let Name = prompt("Enter Name");
+// let email = prompt("enter email");
+
+// if(Name=="bilal" && email=="bilal@gmail.com"){
+//     document.write("name," , Name , ",email," , email );
+// }
+// else{
+//     document.write("invalid user name or email")
+// }
+
+let name = prompt("Ente Name")
+let email = prompt("Enter Email")
+
+
+if(name == "ebad" && email=="ebadulhassan967@gmail.com"){
+    document.write("name",name,"email",email)
+}
+else{
+    document.write("invalid user name or email")
+}
